f8acb861a9e4a2d86ec5c4ce170d022f634afcd8
Mohmotte <71752639+Mohmotte@users.noreply.github.com>
2023-03-08 18:30:51 +0100

    Add files via upload

---
+++ assets/js/menu.js
@@ -0,0 +1,32 @@
+var str="";
+var archives=14;
+var nom=unescape(location.pathname);
+var nom=nom.replace(/\/$/,"");
+var nom=nom.replace(".html","");
+nom=nom.substring(nom.lastIndexOf("/")+1);
+if(nom=="index")
+{
+nom=archives;
+}
+for(var i=1;i<=archives;i++)
+{
+if(i<10)
+{
+i="0"+i;
+}
+str=str+"<a ";
+if(i==nom)
+{
+str=str+"class=\"active\" ";
+}
+str=str+"href=\"";
+if(i<archives)
+{
+str=str+i+".html\">TOME "+i+"</a>";
+}
+else
+{
+str=str+"index.html\">TOME "+i+"</a>";
+}
+}
+document.getElementById("menu").innerHTML = str;

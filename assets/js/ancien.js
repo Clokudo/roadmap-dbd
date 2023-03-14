cc637c74ae34d4ab56a44b3b3a9494b1a39412fd
Mohmotte <71752639+Mohmotte@users.noreply.github.com>
2023-03-14 21:45:42 +0100

    Update ancien.js

--- assets/js/ancien.js
+++ assets/js/ancien.js
@@ -1,3 +1,7 @@
+$("#N01").show();
+$("#N02").show();
+$("#N03").show();
+$("#N04").show();
 var CN04=document.getElementById("N04");
 var LN04=document.getElementById("LN04");
 CN04.classList.add("active");

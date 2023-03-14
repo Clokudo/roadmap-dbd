9d6025d7158d073806e38bbc20b5dd25a8df627b
Mohmotte <71752639+Mohmotte@users.noreply.github.com>
2023-03-14 19:17:31 +0100

    Update actuel.js

--- assets/js/actuel.js
+++ assets/js/actuel.js
@@ -1,1 +1,43 @@
 
+	var N01=new Date(1674662400000);
+	var N02=new Date(1675872000000);
+	var N03=new Date(1677081600000);
+	var N04=new Date(1678892400000);
+	var CN01=document.getElementById("N01");
+	var CN02=document.getElementById("N02");
+	var CN03=document.getElementById("N03");
+	var CN04=document.getElementById("N04");
+	if(new Date()<N01)
+	{
+	$("#N01").hide();
+	}
+	if(new Date()<N02)
+	{
+	CN01.classList.add("active");
+	$("#N02").hide();
+	}
+	else
+	{
+	CN01.classList.remove("active");
+	CN02.classList.add("active");
+	}
+	if(new Date()<N03)
+	{
+	CN02.classList.add("active");
+	$("#N03").hide();
+	}
+	else
+	{
+	CN02.classList.remove("active");
+	CN03.classList.add("active");
+	}
+	if(new Date()<N04)
+	{
+	CN03.classList.add("active");
+	$("#N04").hide();
+	}
+	else
+	{
+	CN03.classList.remove("active");
+	CN04.classList.add("active");
+	}

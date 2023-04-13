05e8eea5329a9ce37ffc0cedca9ba125d330c6aa
Mohmotte <71752639+Mohmotte@users.noreply.github.com>
2023-04-13 06:02:45 +0200

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -105,10 +105,10 @@ for (var i = 0; i < elements.length; i++) {
 }*/
 if(nom==archives)
 {
-var N01=new Date();
-var N02=new Date();
-var N03=new Date();
-var N04=new Date();
+var N01=new Date("2023-05-01");
+var N02=new Date("2023-05-01");
+var N03=new Date("2023-05-01");
+var N04=new Date("2023-05-01");
 var CN01=document.getElementById("N01");
 var CN02=document.getElementById("N02");
 var CN03=document.getElementById("N03");
@@ -121,52 +121,31 @@ if(new Date()>=N01)
 {
 $("#N01").show();
 CN01.classList.add("active");
-if(LN01!=null)
-{
 LN01.classList.add("show");
-}
 }
 if(new Date()>=N02)
 {
 $("#N02").show();
 CN01.classList.remove("active");
-if(LN01!=null)
-{
 LN01.classList.remove("show");
-}
 CN02.classList.add("active");
-if(LN02!=null)
-{
 LN02.classList.add("show");
-}
 }
 if(new Date()>=N03)
 {
 $("#N03").show();
 CN02.classList.remove("active");
-if(LN02!=null)
-{
 LN02.classList.remove("show");
-}
 CN03.classList.add("active");
-if(LN03!=null)
-{
 LN03.classList.add("show");
-}
 }
 if(new Date()>=N04)
 {
 $("#N04").show();
 CN03.classList.remove("active");
-if(LN03!=null)
-{
 LN03.classList.remove("show");
-}
 CN04.classList.add("active");
-if(LN04!=null)
-{
 LN04.classList.add("show");
-}
 }
 }
 else

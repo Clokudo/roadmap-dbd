0fafd88392d926e994208ca13d83d84bb0ea1b71
Mohmotte <71752639+Mohmotte@users.noreply.github.com>
2023-04-13 05:51:27 +0200

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -30,6 +30,10 @@ for (let i = 0; i < spans.length; i++) {
   const id = `N${(i+1).toString().padStart(2, '0')}`;
   spans[i].setAttribute('id', id);
 }
+$("#N01").hide();
+$("#N02").hide();
+$("#N03").hide();
+$("#N04").hide();
 // Sélection de tous les éléments de la classe "level"
 const levels = document.querySelectorAll('.level');
 
@@ -38,10 +42,6 @@ for (let i = 0; i < levels.length; i++) {
   const id = `LN${(i+1).toString().padStart(2, '0')}`;
   levels[i].setAttribute('id', id);
 }
-$("#N01").hide();
-$("#N02").hide();
-$("#N03").hide();
-$("#N04").hide();
 if(nom=="index"||nom=="roadmap-dbd")
 {
 var nom=archives;
